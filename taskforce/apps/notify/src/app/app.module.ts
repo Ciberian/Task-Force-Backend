0a04ea5e994cc63420c8be68a17fb4c3059a24eb
Ciberian <isi04047891@gmail.com>
2022-12-25 14:48:05 +0500

    Исправляет ошибки

--- taskforce/apps/notify/src/app/app.module.ts
+++ taskforce/apps/notify/src/app/app.module.ts
@@ -3,16 +3,18 @@ import { MailerModule } from '@nestjs-modules/mailer';
 import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
-import notifyConfig from '../config/notify.config';
+import notifyConfig, { getSmtpConfig } from '../config/notify.config';
 
 @Module({
   imports: [
-    MailerModule,
     ConfigModule.forRoot({
       cache: true,
       isGlobal: true,
+      envFilePath: 'environments/.notify.env',
       load: [notifyConfig],
-  })],
+    }),
+    MailerModule.forRootAsync(getSmtpConfig()),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- taskforce/apps/notify/src/config/notify.config.ts
+++ taskforce/apps/notify/src/config/notify.config.ts
@@ -1,6 +1,20 @@
-import { registerAs } from '@nestjs/config';
+import { MailerOptions } from '@nestjs-modules/mailer';
+import { ConfigService, registerAs } from '@nestjs/config';
 
 export default registerAs('notify', () => ({
   server: process.env.SMTP_SERVER,
   port: parseInt(process.env.SMTP_SERVER_PORT, 10)
 }));
+
+export function getSmtpConfig() {
+  return {
+    useFactory: async (configService: ConfigService): Promise<MailerOptions> => {
+      return{
+      transport: {
+        host: configService.get<string>('notify.host'),
+        port: configService.get<number>('notify.port'),
+      },
+    }},
+    inject: [ConfigService],
+  };
+}
