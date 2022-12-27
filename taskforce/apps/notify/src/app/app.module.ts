07fe68ae39d69997555e9f622e037130c5f4b720
Ciberian <isi04047891@gmail.com>
2022-12-27 18:40:28 +0500

    Для сервиса notify, выносит путь к env файлу в отдельную переменную

--- taskforce/apps/notify/src/app/app.module.ts
+++ taskforce/apps/notify/src/app/app.module.ts
@@ -4,13 +4,14 @@ import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import notifyConfig, { getSmtpConfig } from '../config/notify.config';
+import { ENV_FILE_PATH } from './app.constant';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       cache: true,
       isGlobal: true,
-      envFilePath: 'environments/.notify.env',
+      envFilePath: ENV_FILE_PATH,
       load: [notifyConfig],
     }),
     MailerModule.forRootAsync(getSmtpConfig()),
