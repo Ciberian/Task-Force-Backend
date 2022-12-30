f62d1911ff43cd314f63290b0003287e57bdec37
Ciberian <isi04047891@gmail.com>
2022-12-30 19:54:02 +0500

    Подготавливает конфигурацию настроек для подключения к MongoDB

--- taskforce/apps/notify/src/app/app.module.ts
+++ taskforce/apps/notify/src/app/app.module.ts
@@ -4,7 +4,10 @@ import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import notifyConfig, { getSmtpConfig } from '../config/notify.config';
+import { getMongoDbConfig, mongoDbOptions } from '../config/mongodb.config';
+import { validateEnvironments } from './env.validation';
 import { ENV_FILE_PATH } from './app.constant';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -12,9 +15,11 @@ import { ENV_FILE_PATH } from './app.constant';
       cache: true,
       isGlobal: true,
       envFilePath: ENV_FILE_PATH,
-      load: [notifyConfig],
+      load: [notifyConfig, mongoDbOptions],
+      validate: validateEnvironments
     }),
     MailerModule.forRootAsync(getSmtpConfig()),
+    MongooseModule.forRootAsync(getMongoDbConfig())
   ],
   controllers: [AppController],
   providers: [AppService],
