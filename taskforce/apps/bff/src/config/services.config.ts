96ce458728ffc15933eead8d1739283a2a58bc81
Ciberian <isi04047891@gmail.com>
2023-01-22 04:10:41 +0500

    Удаляет файлы подготовленные для сервиса bff

--- taskforce/apps/bff/src/config/services.config.ts
+++
@@ -1,8 +0,0 @@
-import { registerAs } from '@nestjs/config';
-
-export const servicesOptions = registerAs('services', () => ({
-  tasks: process.env.TASKS_SERVICE_URL,
-  comments: process.env.COMMENTS_SERVICE_URL,
-  users: process.env.USERS_SERVICE_URL,
-  notify: process.env.NOTIFY_SERVICE_URL,
-}));
